1120b558cb03541088b9b82cc8838acbae65b147
DanTel <DaniyarT2005@gmail.com>
2021-08-08 16:50:59 +0600

    feat: solve morse decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let binaryCode = String(expr).match(/.{2}/g);  //Делит всё по 2 цифры
+    for(let i in binaryCode) {
+        if(binaryCode[i] === '10') {  //Заменяет 10 на точку
+            binaryCode.splice(i, 1, '.');
+        } else if(binaryCode[i] === '11') {  //Заменяет 11 на тире
+            binaryCode.splice(i, 1, '-');
+        } else if(binaryCode[i] === '00') {  //Заменяет 00 на одиночный 0 чтобы во всех буквах были по 5 символов
+            binaryCode.splice(i, 1, '0');
+        } else {  //Заменяет ** на одиночный * чтобы потом при замене ** на пробел не было 2 пробела вместо 1
+            binaryCode.splice(i, 1, '*');
+        }
+    }
+    binaryCode = binaryCode.join('').match(/.{5}/g);  //Делит буквы по 5 символов
+    for(let i in binaryCode) {
+        binaryCode[i] = binaryCode[i].replace(/(0)/g, '');  //Убирает ноли
+        binaryCode.splice(i, 1, MORSE_TABLE[binaryCode[i]]);  //Проверяет на наличие ключей и заменяет ключи значениями
+        if(binaryCode[i] === undefined) {  //Обрабатывает особый случай(пробел *****) и меняет его на ' '
+            binaryCode.splice(i, 1, ' ');
+        }
+    }
+    return binaryCode.join('');  //Собирает массив в строку
 }
 
 module.exports = {
